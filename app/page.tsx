798d3852874e41728e470bab5067159b6fc0e233
Mark Yu <zyu347@uwo.ca>
2024-07-18 23:36:33 -0400

    added components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
+import dynamic from "next/dynamic";
 
-export default function Page() {
-  return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
-  );
-}
+/**
+ * Disable SSR to avoid pre-rendering issues of Next.js
+ *
+ * We're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server.
+ */
+const App = dynamic(() => import("./App"), { ssr: false });
+
+export default App;
